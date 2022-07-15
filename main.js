ce842d9a65e5a3a149ade1422ddaa7347cd566ce
fchandiac <felipe.chandia.cast@gmail.com>
2022-07-15 05:43:30 -0400

    viernes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 app.set('json spaces', 2)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -10,7 +10,7 @@ const cors = require('cors')
 
 app.use(cors({ origin: '*' }))
 app.use(require('./routes/persons'))
-app.use(express.static(__dirname + '/out/'))
+app.use(express.static(__dirname + '/out'))
 
 app.get('/data',function(req,res) {
     res.sendFile(__dirname + '/out/data.html');
